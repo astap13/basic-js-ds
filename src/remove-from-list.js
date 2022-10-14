3e5f458a11f02f13741f7477bbae3be346f5ba61
Kiryl Astapenka <81929875+astap13@users.noreply.github.com>
2022-10-14 21:10:28 +0200

    Add files via upload

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -22,9 +22,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(l, k) {
+  while (l.value === k) {
+    l = l.next;
+  }
+  let current = l;
+  let prev = null;
+  while (current) {
+    current.value === k ? prev.next = current.next : prev = current;
+    current = current.next;
+  }
+  return l;
 }
 
 module.exports = {
